f753e0134818cae5e7dfd81b19b792ff62fc785f
ngoquigiau <ngovo538962@gmail.com>
2017-04-28 09:58:29 +0700

    Update chongcopy.js

--- chongcopy.js
+++ chongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/C%C4%83n-h%E1%BB%99-centana-th%E1%BB%A7-thi%C3%AAm-1253971348031947/'>https://www.facebook.com/C%C4%83n-h%E1%BB%99-centana-th%E1%BB%A7-thi%C3%AAm-1253971348031947/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://centanatruongluu.blogspot.com/'>http://centanatruongluu.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
